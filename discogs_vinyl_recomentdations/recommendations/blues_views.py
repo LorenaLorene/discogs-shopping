7e463578c8ab2d10163fe249606b8430726d9bc9
Lorena <Lorenija@gmail.com>
2019-12-13 18:27:39 +0000

    creating env and requirements

--- discogs_vinyl_recomentdations/recommendations/blues_views.py
+++ discogs_vinyl_recomentdations/recommendations/blues_views.py
@@ -1,5 +1,6 @@
 from django.http.response import JsonResponse
 import discogs_client
+import json
 import pandas as pd
 
 discogs_api = discogs_client.Client('my_user_agent/1.0', user_token='iveLOjvMtgyHUUCuyAzElYGLTuWzBeyiOhpjAEYA')
@@ -7,16 +8,19 @@ discogs_api = discogs_client.Client('my_user_agent/1.0', user_token='iveLOjvMtgy
 
 def index(request):
     results = discogs_api.search('Sell', genre='Blues', format='Vinyl',
-                                 currency='GBP', price='5to10', year='1991')
+                                 currency='GBP', price='5to10')
+
+    # results2 = discogs_api.artist(id='322292')
+
     all_dicts = []
-    print(len(results))
     for result in results:
         dict1 = {}
         dict1['title'] = result.title
         all_dicts.append(dict1)
+        # all_dicts = pd.array()
     return JsonResponse(all_dicts, json_dumps_params={'indent': 2}, safe=False)
     # return all_dicts
 
 
-a = pd.read_json()
-print(a)
+# a = pd.read_json()
+# print(a)
